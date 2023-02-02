2a3d9dd46ac236aca99014ce12a2aa8d66944a35
naynaingoo2003 <117724125+naynaingoo2003@users.noreply.github.com>
2023-02-02 20:42:32 +0630

    Create Dimensional_Array.js

---
+++ Dimensional_Array.js
@@ -0,0 +1,13 @@
+// Dimensional Array
+	const numberOne = [1,2,3,4,5,6,7,8]; //One Dimensional Array
+	
+	const numberTwo = [[1,2], [3,4], [5,6], [7,8]]; //Two Dimensional
+
+	console.log(numberOne[0]); // 1
+
+	console.log(numberTwo[0]); //[1,2]
+
+	//Accsee from Two Dimensional
+	let number = numberTwo[3];
+	let x = number[0];
+	console.log(x); // 7
